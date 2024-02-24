c320544d3d90a371381bce7d70934c943b1a88cb
anjaliJT <anjali.johnnette@gmail.com>
2024-02-24 18:26:55 +0530

    Fixed each time layout creation

--- browse.py
+++ browse.py
@@ -5,12 +5,9 @@ class PDFViewerWidget(QtWidgets.QWidget):
     def __init__(self, parent=None,pdf_path=None):
         super().__init__(parent)
         self.pdf_path = pdf_path
-        # self.setup_ui()
+        self.setup_ui()
 
-    def setup_ui(self,Path=None):
-        if Path is not None:
-            self.pdf_path = Path
-            
+    def setup_ui(self):   
         layout = QtWidgets.QVBoxLayout(self)
         self.webview = QtWebEngineWidgets.QWebEngineView()
         layout.addWidget(self.webview)
@@ -19,12 +16,14 @@ class PDFViewerWidget(QtWidgets.QWidget):
             QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
         self.webview.settings().setAttribute(
             QtWebEngineWidgets.QWebEngineSettings.PdfViewerEnabled, True)
-        self.load_pdf()
+        # self.load_pdf()
 
-    def load_pdf(self):
+    def load_pdf(self,path):
+        self.pdf_path = path 
         self.webview.load(QtCore.QUrl.fromUserInput(self.pdf_path))
 
 if __name__ == '__main__':
+    
     app = QtWidgets.QApplication(sys.argv)
     pdf_path = r"c:\Users\pk\Downloads\Study material Pdfs\let-us-python-3nbsped.pdf"
     window = PDFViewerWidget(pdf_path=pdf_path)
