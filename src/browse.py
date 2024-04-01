db94b483f73ab1f91417845e548df6ad94d427b6
anjaliJT <anjali.johnnette@gmail.com>
2024-04-01 18:27:08 +0530

    Removed margin

--- src/browse.py
+++ src/browse.py
@@ -11,12 +11,13 @@ class PDFViewerWidget(QtWidgets.QWidget):
         layout = QtWidgets.QVBoxLayout(self)
         self.webview = QtWebEngineWidgets.QWebEngineView()
         layout.addWidget(self.webview)
-
+        layout.setContentsMargins(0, 0, 0, 0)  # Set margins to zero
+        layout.setSpacing(0)  # Set spacing to zero
         self.webview.settings().setAttribute(
             QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
         self.webview.settings().setAttribute(
             QtWebEngineWidgets.QWebEngineSettings.PdfViewerEnabled, True)
-        # self.load_pdf()
+        self.load_pdf(self.pdf_path)
 
     def load_pdf(self,path):
         self.pdf_path = path 
